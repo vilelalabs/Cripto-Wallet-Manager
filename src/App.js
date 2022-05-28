366937cabe533599ac562dd6963eefe220c3df2a
hvilela0 <hvilela0@gmail.com>
2022-05-28 16:09:22 -0300

    added Total Balance field

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { styles } from './styles';
 import { Header } from './components/Header';
 import { Widget } from './components/Widget';
 import { CriptoList } from './components/CriptoList';
+import { TotalBalance } from './components/TotalBalance';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
       <Header />
-      <CriptoList/>
+      <CriptoList />
+      <TotalBalance/>
       <Widget />
     </View>
   );
